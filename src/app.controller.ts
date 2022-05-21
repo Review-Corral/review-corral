89b910313ca03ce2b3ad73a16be42fa96e7795ba
Alex McLean <alex.mclean@hey.com>
2022-05-21 08:42:52 -0700

    Adding some types

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { GithubActions, GithubEvent } from 'types/github';
 import { AppService } from './app.service';
 
+const pullRequests = [];
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +10,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/events')
+  postGithubEvents(@Body() body: GithubEvent) {
+    console.log(`Got action of ${body.action}`);
+
+    if (GithubActions.includes(body.action)) {
+      console.log(`Got action of ${body.action}`);
+    }
+
+    console.log(JSON.stringify(body, null, 2));
   }
 }
