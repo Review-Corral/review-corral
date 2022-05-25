eacd0d91cdf4e42a4afebf8a696170238fcd364a
Alex McLean <alex.mclean@hey.com>
2022-05-24 21:13:41 -0700

    Github events

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -1,10 +1,5 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import {
-  GithubAction,
-  GithubActions,
-  GithubEvent,
-  PullRequest,
-} from "types/githubApiTypes";
+import { Body, Controller, Post } from "@nestjs/common";
+import { GithubEvent, PullRequest } from "types/githubApiTypes";
 import { GithubService } from "./github.service";
 
 // We just need to map the pull requests with the slack IDs so we can thread the messages
@@ -14,26 +9,9 @@ const pullRequests: Record<PullRequest["id"], PullRequest> = {};
 export class GithubController {
   constructor(private readonly githubService: GithubService) {}
 
-  @Get()
-  getHello(): string {
-    return this.githubService.getHello();
-  }
-
   @Post("/events")
   postGithubEvents(@Body() body: GithubEvent) {
-    console.log(`Got action of ${body.action}`);
-
-    if (GithubActions.includes(body.action)) {
-      const action: GithubAction = body.action;
-
-      console.log(`Got action of ${body.action}`);
-
-      if (action === "opened") {
-        pullRequests[body.pull_request.id] = body.pull_request;
-      }
-    }
-
-    console.log(JSON.stringify(body, null, 2));
+    this.githubService.handleEvent(body);
   }
 
   @Post("/send-message")
--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,18 +1,52 @@
 import { Injectable } from "@nestjs/common";
 import { WebClient } from "@slack/web-api";
+import { GithubEvent } from "types/githubApiTypes";
 // delete-this comment
 
 @Injectable()
 export class GithubService {
   slackClient: WebClient;
+
+  // ID of the PR to ID of the message
+  seenPrs: Record<string, string> = {};
+
   constructor() {
-    console.log("Instantiating");
-    console.log("token: ", process.env.SLACK_BOT_TOKEN);
     this.slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
   }
 
-  getHello(): string {
-    return "Hello c!";
+  handleEvent(body: GithubEvent) {
+    if (body.pull_request && body.pull_request.id) {
+      const prId = body.pull_request.id;
+      if (prId in this.seenPrs) {
+        this.slackClient.chat.postMessage({
+          token: process.env.SLACK_BOT_TOKEN,
+          thread_ts: this.seenPrs[prId],
+          channel: "C03GBF3FCNR",
+          text: `${body.sender.login} ${body.action} <${body.pull_request.html_url}|${body.pull_request.title}>`,
+          attachments: [
+            {
+              author_name: "Bobby Tables",
+              color: "#2eb886",
+            },
+          ],
+        });
+      } else {
+        this.slackClient.chat
+          .postMessage({
+            token: process.env.SLACK_BOT_TOKEN,
+            channel: "C03GBF3FCNR",
+            text: `${body.sender.login} ${body.action} <${body.pull_request.html_url}|${body.pull_request.title}>`,
+          })
+          .then((message) => {
+            console.log(`Message thread_ts: ${message.message?.thread_ts}`);
+            if (message.message?.ts) {
+              this.seenPrs[prId] = message.message.ts;
+              console.log("Added pr id to seenPrs");
+              console.log(this.seenPrs);
+            }
+          });
+      }
+    }
   }
 
   sendMessage() {
