52fc52f6cea48b77c75459579f2974f2e75576e3
Alex McLean <alex.mclean@hey.com>
2022-05-30 16:50:51 -0700

    Get Slack access token

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -1,13 +1,39 @@
 import { Body, Controller, Get, Query } from "@nestjs/common";
+import axios from "axios";
+import { RootObject } from "types/slackAuthTypes";
 
 @Controller("/slack")
 export class SlackController {
   @Get()
   getSlackAuthEvent(
     @Body() body: any,
-    @Query() query: { name: string; state?: string },
+    @Query() query: { code: string; state?: string },
   ) {
     console.log(body);
     console.log("query: ", query);
+
+    // fetch("https://slack.com/api/oauth.v2.access", {
+    //   body: `client_id=${process.env.SLACK_BOT_ID}&code=${query.code}&client_secret=${process.env.SLACK_SIGNING_SECRET}`,
+    //   headers: {
+    //     "Content-Type": "application/x-www-form-urlencoded",
+    //   },
+    //   method: "post",
+    // });
+
+    axios
+      .postForm<
+        {
+          client_id: string;
+          code: string;
+          client_secret: string;
+        },
+        RootObject
+      >("https://slack.com/api/oauth.v2.access", {
+        client_id: process.env.SLACK_BOT_ID,
+        code: query.code,
+        client_secret: process.env.SLACK_SIGNING_SECRET,
+      })
+      .then((response) => console.log(response))
+      .catch((e) => console.log(e));
   }
 }
