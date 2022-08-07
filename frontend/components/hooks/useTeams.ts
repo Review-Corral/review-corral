316a8c6eb4c28ecb3f2da150c243036f4a945c2d
Alex McLean <alex.mclean@hey.com>
2022-08-06 18:07:53 -0700

    Giving team id to button

--- frontend/components/hooks/useTeams.ts
+++ frontend/components/hooks/useTeams.ts
@@ -1,8 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+
+export type Team = {
+  created_at: Date | null;
+  id: string;
+  name: string | null;
+};
 
 export const useTeams = () => {
-  return useQuery(["getTeams"], async () => {
+  return useQuery<Team[], AxiosError>(["getTeams"], async () => {
     return (await axios.get("/api/proxy/teams")).data;
   });
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import { withPageAuth } from "@supabase/auth-helpers-nextjs";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import GithubButton from "../components/GithubButton";
 import { useTeams } from "../components/hooks/useTeams";
 import { DashboardLayout } from "../components/layout/DashboardLayout";
 
@@ -9,13 +10,23 @@ const teamId = "7611d060-35ee-401f-8e99-58b2f7a9849d";
 const Home: NextPage = () => {
   const router = useRouter();
   const teams = useTeams();
+
+  if (teams.isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <DashboardLayout>
-      <h1>Welcome to Review Corral!</h1>
       <div className="space-y-6">
-        {JSON.stringify(teams.data, null, 2)}
-        {/* <GithubButton state={""} />
-        <SlackButton /> */}
+        <h1>Welcome to Review Corral!</h1>
+        {!teams.data || teams.data.length < 1 ? (
+          <div>No team!</div>
+        ) : (
+          <div className="space-y-6">
+            <GithubButton state={teams.data[0].id} />
+            {/* <SlackButton /> */}
+          </div>
+        )}
       </div>
     </DashboardLayout>
   );
@@ -23,4 +34,7 @@ const Home: NextPage = () => {
 
 export default Home;
 
-withPageAuth();
+export const getServerSideProps = withPageAuth({
+  authRequired: true,
+  redirectTo: "/login",
+});
