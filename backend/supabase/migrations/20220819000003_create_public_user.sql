7a53140678a034b112bc583c28c67c2268b80e2d
Alex McLean <alex.mclean@hey.com>
2022-08-20 11:35:28 -0700

    Lots of seed scripts

---
+++ backend/supabase/migrations/20220819000003_create_public_user.sql
@@ -0,0 +1,15 @@
+/**
+* This trigger automatically creates a user entry when a new user signs up via Supabase Auth.
+*/
+
+create function public.handle_new_user() 
+returns trigger as $$
+begin
+  insert into public.users (id)
+  values (new.id);
+  return new;
+end;
+$$ language plpgsql security definer;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
