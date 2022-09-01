36c9e07bf824cda407c8116c6fedb2a5ae257ec9
Alex McLean <alex.mclean@hey.com>
2022-09-01 11:29:32 -0700

    No retries

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           queries: {
             refetchOnWindowFocus: false,
             refetchOnReconnect: false,
+            retry: 0,
           },
         },
       }),
