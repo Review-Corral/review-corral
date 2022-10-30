f14b06fd6fa3bda1ca36a48f7a495e97b0326867
Alex McLean <alex.mclean@hey.com>
2022-10-30 13:30:54 -0700

    Cleaning up installed repos component

--- frontend/components/teams/repos/InstalledReposContent.tsx
+++ frontend/components/teams/repos/InstalledReposContent.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { ErrorAlert } from "../../common/alerts/Error";
 import { Toggle } from "../../common/Toggle";
 import {
   useGetInstallationRepos,
@@ -14,59 +15,30 @@ export const InstalledReposContent: FC<InstalledReposContentProps> = ({
 }) => {
   const getInstalledRepos = useGetInstallationRepos(installationId);
   const mutateRepo = useMutateInstallationRepo({ installationId });
-  // const getSyncedRepos = useGetSyncedRepos(installationId);
-  // const syncRepo = useSyncRepo(installationId);
-  // const deleteSyncedRepo = useDeleteSyncedRepo(installationId);
 
-  // if (getSyncedRepos.isLoading && getInstalledRepos.isLoading) {
-  //   return <div>Loading installed &amp; repods</div>;
-  // }
+  if (getInstalledRepos.isLoading) {
+    return <div>Loading installed &amp; repods</div>;
+  }
 
-  // if (getInstalledRepos.error) {
-  //   return (
-  //     <ErrorAlert
-  //       message={"Error getting installed repositories"}
-  //       subMessage={
-  //         getInstalledRepos.error.response?.status === 401 && (
-  //           <>
-  //             Try{" "}
-  //             <a
-  //               className="underline cursor-pointer"
-  //               href={getGithubAuthorizationUrl(installationId).toString()}
-  //             >
-  //               re-authenticating
-  //             </a>
-  //           </>
-  //         )
-  //       }
-  //     />
-  //   );
-  // }
+  if (getInstalledRepos.error) {
+    return <ErrorAlert message={"Error getting installed repositories"} />;
+  }
 
   if (!getInstalledRepos.data || getInstalledRepos.data.length === 0) {
     return <div>No installed repositories found</div>;
   }
 
-  // if (!getSyncedRepos.data) {
-  //   throw Error("Finding synchronized repos failed.");
-  // }
-
   const installedRepos = getInstalledRepos.data;
-  // const syncedRepos = getSyncedRepos.data;
 
   return (
     <div>
       <div className="space-y-2 max-w-lg">
         {installedRepos.map((repository) => {
-          // const syncedRepo = syncedRepos.find(
-          //   (s) => s.repository_id === repository.id.toString(),
-          // );
           return (
             <div key={repository.id} className="grid grid-cols-2">
               <div>{repository.repository_name}</div>
               <Toggle
                 isEnabled={repository.is_active}
-                // isEnabled={!!syncedRepo}
                 onToggle={(isEnabled) => {
                   mutateRepo.mutate({
                     repositoryId: repository.repository_id,
