4e21657aefc7b072a7928853c9b75521d0e07e5b
Alex McLean <alex.mclean@hey.com>
2022-10-29 21:23:16 -0700

    Github auth looping back but no user being saved somehow

--- frontend/pages/api/gh/local-auth.ts
+++ frontend/pages/api/gh/local-auth.ts
@@ -1,12 +1,19 @@
-import { withApiAuth } from "@supabase/auth-helpers-nextjs";
+import withApiSupabase from "../../../components/api/utils/withApiSupabase";
 import { Database } from "../../../database-types";
 
 // Utility to redirect Github OAuth requests from Ngrok to the correct URL
-export default withApiAuth<Database>(async function ProtectedRoute(
+export default withApiSupabase<Database>(async function ProtectedRoute(
   req,
   res,
   _,
 ) {
-  console.log("Got request to GET /api/gh/local-auth");
-  res.redirect(307, "http://localhost:54321/auth/v1/callback");
+  console.log(`Got request to ${req.method} /api/gh/local-auth`);
+  console.log("req.query", req.query);
+  const params = new URLSearchParams(
+    req.query as Record<string, string>,
+  ).toString();
+
+  console.log("generated params: ", params);
+
+  res.redirect(307, "http://localhost:54321/auth/v1/callback?" + params).end();
 });
