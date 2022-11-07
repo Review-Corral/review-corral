b4533c12929298cfee21d4be71e36d2ab2bb7b50
Alex McLean <alex.mclean@hey.com>
2022-11-06 16:18:06 -0800

    More logging in members query

--- pages/api/gh/[orgId]/members.ts
+++ pages/api/gh/[orgId]/members.ts
@@ -10,9 +10,11 @@ import {
 
 export default withAxiom(
   withProtectedApi(async function GetMembers(req, res, supabaseServerClient) {
+    req.log.debug("Get memebrs query called");
     const orgId = flattenParam(req.query.orgId);
 
     if (!orgId) {
+      req.log.info("Members call returning 404 since no orgId provided");
       return res.status(404).send({ error: "No orgId provided" });
     }
 
@@ -24,7 +26,7 @@ export default withAxiom(
       .single();
 
     if (error || !organization) {
-      req.log.error("Error getting organization: ", error);
+      req.log.error("Error getting members: ", error);
       return res.status(404).send({
         error: `No organization found for id: ${orgId} ${
           error && ". Error: " + error.message
@@ -33,9 +35,12 @@ export default withAxiom(
     }
 
     if (organization.organization_type !== "Organization") {
-      req.log.info("Organization is not of organization type (no members): ", {
-        orgId: organization.id,
-      });
+      req.log.info(
+        "Getting members: Organization is not of organization type (no members): ",
+        {
+          orgId: organization.id,
+        },
+      );
       return res.status(400).send({
         error: "Can't get members for non-organization Organization type",
       });
@@ -45,7 +50,7 @@ export default withAxiom(
       organization.installation_id,
     ).catch((error) => {
       req.log.error(
-        `No installation access token found for installation id: ${
+        `Getting members: No installation access token found for installation id: ${
           organization.installation_id
         } ${error && ". Error: " + error.message}`,
       );
@@ -53,7 +58,9 @@ export default withAxiom(
     });
 
     if (installationAccessToken) {
-      req.log.info("Got installation access token for members query");
+      req.log.info(
+        "Getting members: Got installation access token for members query",
+      );
 
       const members = await getOrganizationMembers(
         organization.account_name,
@@ -62,7 +69,10 @@ export default withAxiom(
       );
 
       if (members) {
-        req.log.info("Got members for organization ", organization);
+        req.log.info("Got members for organization ", {
+          organization,
+          members,
+        });
         return res.status(200).send({ data: members });
       } else {
         req.log.warn("No members found for organization: ", organization);
