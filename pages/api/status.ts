48af0447d45509ca898c6ea9babd5690102bfc4b
Alex McLean <alex.mclean@hey.com>
2022-11-10 07:36:44 -0800

    Response on status OK

--- pages/api/status.ts
+++ pages/api/status.ts
@@ -3,7 +3,7 @@ import { AxiomAPIRequest, withAxiom } from "next-axiom/dist/withAxiom";
 
 const handler = (req: AxiomAPIRequest, res: NextApiResponse) => {
   req.log.info("Called Status API check");
-  res.status(200).end();
+  res.status(200).send({ data: "OK" });
 };
 
 export default withAxiom(handler);
