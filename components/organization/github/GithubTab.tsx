52867f69670d3436b57c2a3fee5830edf19601b2
Alex McLean <alex.mclean@hey.com>
2022-11-22 16:09:46 -0800

    Making table format better for smaller screens

--- components/organization/github/GithubTab.tsx
+++ components/organization/github/GithubTab.tsx
@@ -22,8 +22,8 @@ export const GithubTab: FC<OrgViewProps> = ({ organization }) => {
         />
       ) : (
         <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+          <div className="overflow-x-auto -mx-6">
+            <div className="inline-block min-w-full py-2 align-middle px-6">
               <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-300">
                   <thead className="bg-gray-50">
