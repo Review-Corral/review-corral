7777e0f94cb71feded182869234a066b8bacb300
Alex McLean <alex.mclean@hey.com>
2023-08-07 08:28:21 -0700

    Commenting out supabase_vault

--- supabase/migrations/20230807150543_remote_commit.sql
+++ supabase/migrations/20230807150543_remote_commit.sql
@@ -26,7 +26,7 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
 
 CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
 
-CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+-- CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
