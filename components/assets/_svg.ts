d1c798595e46cc45c1887d0a3038b1ed34359a61
Alex McLean <alex.mclean25@gmail.com>
2023-12-24 10:22:53 -0800

    SST init

--- components/assets/_svg.ts
+++
@@ -1,6 +0,0 @@
-export interface SVGProps {
-  className?: string;
-  onClick?: () => void;
-  // Pass all props to underlying <svg> tag
-  [prop: string]: any;
-}
--- components/utils/flattenParam.ts
+++
@@ -1,5 +0,0 @@
-export function flattenParam(param?: string | string[]): string | null {
-  if (!param) return null;
-
-  return Array.isArray(param) ? param[0] : param;
-}
