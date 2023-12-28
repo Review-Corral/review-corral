14f79a9a4bfade09b62a3847b075554d0179ea9b
Alex McLean <alex.mclean25@gmail.com>
2023-12-28 12:07:55 -0800

    Adding frontend stack

---
+++ stacks/FrontendStack.ts
@@ -0,0 +1,24 @@
+import { StackContext, StaticSite, use } from "sst/constructs";
+
+import { MainStack } from "./MainStack";
+
+export function FrontendStack({ stack, app }: StackContext) {
+  const { api } = use(MainStack);
+
+  // Define our React app
+  const site = new StaticSite(stack, "ReactSite", {
+    path: "packages/frontend",
+    buildCommand: "pnpm run build",
+    buildOutput: "dist",
+    // Pass in our environment variables
+    environment: {
+      VITE_API_URL: api.url,
+      VITE_REGION: app.region,
+    },
+  });
+
+  // Show the url in the output
+  stack.addOutputs({
+    SiteUrl: site.url,
+  });
+}
