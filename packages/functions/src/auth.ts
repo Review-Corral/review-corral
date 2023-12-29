9ea3d911b3ce51b6642e3403cc0e73112200fed1
Alex McLean <alex.mclean25@gmail.com>
2023-12-29 15:04:18 -0800

    Can fetch user profile data now

--- packages/functions/src/auth.ts
+++ packages/functions/src/auth.ts
@@ -1,5 +1,9 @@
+import ky from "ky";
 import { AuthHandler, GithubAdapter } from "sst/node/auth";
+import { Logger } from "../../core/logging";
 import { assertVarExists } from "../../core/utils/assert";
+
+const LOGGER = new Logger("functions:auth");
 
 export const handler = AuthHandler({
   providers: {
@@ -8,8 +12,21 @@ export const handler = AuthHandler({
       clientSecret: assertVarExists("GH_CLIENT_SECRET"),
       scope: "user",
       onSuccess: async (tokenset) => {
-        console.log("IN ON AUTH SUCCESS");
+        LOGGER.info("Successfully authenticated with GitHub");
         console.log({ tokenset });
+
+        LOGGER.info("Going to fetch user profile information");
+
+        const response = await ky
+          .get("https://api.github.com/user", {
+            headers: {
+              Authorization: `token ${tokenset.access_token}`,
+            },
+          })
+          .json();
+
+        LOGGER.debug("Users fetch response: ", response);
+
         return {
           statusCode: 200,
           body: JSON.stringify(tokenset),
