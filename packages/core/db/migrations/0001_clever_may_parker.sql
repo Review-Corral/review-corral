4c4b42ccb2e4c2995522d137f9413027b7d8fcc7
Alex McLean <alex.mclean25@gmail.com>
2023-12-29 17:50:25 -0800

    Using numbers for Ids of Github resources to match Github

--- packages/core/db/migrations/0001_clever_may_parker.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "id" SET DATA TYPE bigint;--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN IF EXISTS "gh_refresh_token";--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_email_unique" UNIQUE("email");
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Migrations
+
+1. Create migration scripts by using the "Generate Migrations" task in the VSCode actions.
+1. To run the migrations locally, open the SST Console and invoke the `migrateToLatest` function.
