4df9852b6dec7f52610fb77aa62c00fe948df994
Alex McLean <alex.mclean25@gmail.com>
2024-01-04 21:57:04 -0800

    Overview tab working

--- packages/frontend/src/organization/OrgView.tsx
+++ packages/frontend/src/organization/OrgView.tsx
@@ -1,16 +1,45 @@
 import { Organization } from "@core/db/types";
-import { FC, useEffect, useState } from "react";
-import { redirect, useLoaderData, useSearchParams } from "react-router-dom";
+import { FC, ReactNode, useEffect, useState } from "react";
+import {
+  redirect,
+  useLoaderData,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 import { DashboardLayout } from "src/layouts/DashboardLayout";
 import { useOrganizations } from "src/org/useOrganizations";
 import * as z from "zod";
-import OrgTabbedViewHandler from "./OrgTabbedViewHandler";
+import { OverviewTab } from "./tabs/OverviewTab";
 
 interface OrgViewProps {}
 
 const PageSchema = z.enum(["github", "slack", "usernames", "overview"]);
 
 export type Page = z.infer<typeof PageSchema>;
+
+type SubNav = {
+  text: string;
+  page: Page;
+};
+
+const routes: SubNav[] = [
+  {
+    text: "Overview",
+    page: "overview",
+  },
+  {
+    text: "Github",
+    page: "github",
+  },
+  {
+    text: "Slack",
+    page: "slack",
+  },
+  {
+    text: "Usernames",
+    page: "usernames",
+  },
+];
 
 const orgViewParamsSchema = z.object({
   orgId: z.string(),
@@ -30,7 +59,6 @@ export const OrgView: FC<OrgViewProps> = () => {
   const [searchParams, _] = useSearchParams();
 
   const { page } = orgViewSearchParamsSchema.parse(searchParams);
-
   const { orgId } = orgViewParamsSchema.parse(loaderData);
 
   const { data, isLoading } = useOrganizations();
@@ -38,6 +66,24 @@ export const OrgView: FC<OrgViewProps> = () => {
   const [organization, setOrganization] = useState<Organization | undefined>(
     undefined
   );
+
+  const navigate = useNavigate();
+  const [_page, setPage] = useState<Page>(page);
+
+  const setPageWrapper = (page: Page): void => {
+    if (organization) {
+      if (page !== _page) {
+        setPage(page);
+        let route = "";
+        if (page) {
+          route = `/org/${organization.id}?page=${page}`;
+        } else {
+          route = `/org/${organization.id}`;
+        }
+        navigate(route);
+      }
+    }
+  };
 
   // Gets the correct organization from the list of organizations fetched from
   // the database
@@ -56,14 +102,61 @@ export const OrgView: FC<OrgViewProps> = () => {
   }, [data]);
 
   return (
-    <DashboardLayout title="Organization">
+    <DashboardLayout
+      title="Organization"
+      activeOrganizationAccountId={organization?.id}
+      subnav={
+        <>
+          <div className="max-w-7xl mx-auto px-6 pt-4 pb-3 font-medium ">
+            <ul>
+              {routes.map((route) => (
+                <li
+                  key={route.text}
+                  className={`
+                    inline px-2 py-1 cursor-pointer text-base hover:bg-gray-100 rounded-md
+                    `}
+                  onClick={() => setPageWrapper(route.page)}
+                >
+                  <span
+                    className={`
+                      pb-[0.9rem]
+                      px-1
+                      ${
+                        _page == route.page
+                          ? "border-b-2 border-indigo-500"
+                          : ""
+                      }
+                    `}
+                  >
+                    {route.text}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      }
+    >
       {isLoading && <div>Loading...</div>}
-      {organization && (
-        <OrgTabbedViewHandler
-          organization={organization}
-          page={page}
-        ></OrgTabbedViewHandler>
-      )}
+      {!isLoading &&
+        organization &&
+        ((): ReactNode => {
+          const tabProps = { organization, onEdit: setPageWrapper };
+
+          switch (_page) {
+            case "github":
+              return <div>Github</div>;
+            // return <GithubTab {...tabProps} />;
+            case "slack":
+              return <div>Slack</div>;
+            // return <SlackTab {...tabProps} />;
+            case "usernames":
+              return <div>Usernames</div>;
+            // return <UsernamesTab {...tabProps} />;
+            default:
+              return <OverviewTab {...tabProps} />;
+          }
+        })()}
     </DashboardLayout>
   );
 };
