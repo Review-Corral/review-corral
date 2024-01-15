396ab6419cf8c64609b1a6321b45d6e5f2b68814
Alex McLean <alex.mclean25@gmail.com>
2024-01-14 19:24:55 -0800

    Middleware working well

--- packages/web/app/dashboard/actions.ts
+++ packages/web/app/dashboard/actions.ts
@@ -1,14 +1,23 @@
 "use server";
 
 import { User } from "@core/db/types";
+import { Logger } from "@core/logging";
 import ky from "ky";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const LOGGER = new Logger("dashboard:actions");
 
 export async function useUser() {
   const cookieStore = cookies();
   const authToken = cookieStore.get("authToken");
 
-  console.log({ authToken });
+  // Middleware should effectively ensure that this token is set,
+  // but checking anyways
+  if (!authToken?.value) {
+    LOGGER.info("No auth token found, redirecting to login");
+    redirect("/login");
+  }
 
   const user = await ky
     .get(`${process.env.NEXT_PUBLIC_API_URL}/profile`, {
--- packages/web/middleware.ts
+++ packages/web/middleware.ts
@@ -1,12 +1,17 @@
-import { NextRequest } from "next/server";
+import { Logger } from "@core/logging";
+import { NextRequest, NextResponse } from "next/server";
+
+const LOGGER = new Logger("middleware");
 
 export function middleware(request: NextRequest) {
-  console.log("Hello from middleware");
-  if (!request.nextUrl.pathname.startsWith("/login")) {
-    // This logic is only applied to /about
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    const cookies = request.cookies;
+    const authtoken = cookies.get("authToken");
+    if (!authtoken) {
+      LOGGER.info("No auth token found, redirecting to login");
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
 
-  const cookies = request.cookies;
-  const authtoken = cookies.get("authToken");
   // console.log({ authtoken });
 }
