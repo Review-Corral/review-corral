8c1165dc277eb972550bc5fbece6cf7a174be586
Alex McLean <alex.mclean25@gmail.com>
2024-02-03 09:53:45 -0800

    Adding first entities

--- packages/core/dynamodb/orgnization.ts
+++ packages/core/dynamodb/orgnization.ts
@@ -1,55 +1,73 @@
 import { CreateEntityItem, Entity, EntityItem } from "electrodb";
-import { Dynamo } from "./dynamo";
 
-export const OrganizationEntity = new Entity(
-  {
-    model: {
-      version: "1",
-      entity: "Organization",
-      service: "scratch",
+export const OrganizationEntity = new Entity({
+  model: {
+    version: "1",
+    entity: "Organization",
+    service: "scratch",
+  },
+  attributes: {
+    orgId: {
+      type: "number",
+      required: true,
+      readOnly: true,
     },
-    attributes: {
-      orgId: {
-        type: "number",
-        required: true,
-        readOnly: true,
+    name: {
+      type: "string",
+      required: true,
+    },
+    avatarUrl: {
+      type: "string",
+      required: true,
+    },
+    createdAt: {
+      type: "string",
+      required: true,
+      readOnly: true,
+    },
+    type: {
+      type: ["User", "Organization"] as const,
+      required: true,
+      readOnly: true,
+    },
+  },
+  indexes: {
+    primary: {
+      pk: {
+        field: "pk",
+        composite: ["orgId"],
       },
-      name: {
-        type: "string",
-        required: true,
-      },
-      avatarUrl: {
-        type: "string",
-        required: true,
-      },
-      createdAt: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      type: {
-        type: ["User", "Organization"] as const,
-        required: true,
-        readOnly: true,
+      sk: {
+        field: "sk",
+        composite: [],
       },
     },
-    indexes: {
-      primary: {
-        pk: {
-          field: "pk",
-          composite: [],
-        },
-        sk: {
-          field: "sk",
-          composite: ["orgId"],
-        },
+    byName: {
+      pk: {
+        field: "pk",
+        composite: ["orgId"],
+      },
+      sk: {
+        field: "sk",
+        composite: ["name"],
+      },
+    },
+    pullRequests: {
+      collection: "pullRequests",
+      index: "gsi2",
+      pk: {
+        field: "gsi2pk",
+        composite: ["orgId"],
+      },
+      sk: {
+        field: "gsi2sk",
+        composite: [],
       },
     },
   },
-  Dynamo.Configuration
-);
+});
 
-export type ArticleEntityType = EntityItem<typeof OrganizationEntity>;
+export type OrganizationEntityType = EntityItem<typeof OrganizationEntity>;
 
 export async function create(
   props: Omit<CreateEntityItem<typeof OrganizationEntity>, "createdAt">
@@ -63,13 +81,19 @@ export async function create(
 }
 
 export async function get(id: number) {
-  const result = await OrganizationEntity.get({ orgId: id }).go();
+  const result = await OrganizationEntity.query
+    .primary({
+      orgId: id,
+    })
+    .go();
 
   return result.data;
 }
 
-export async function list() {
-  const result = await OrganizationEntity.query.primary({}).go();
+// export async function list() {
+//   const result = await OrganizationEntity.query.primary({
 
-  return result.data;
-}
+//   }).go();
+
+//   return result.data;
+// }
