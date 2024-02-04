f80a2de4c199086544b67ad609c6297a99becff0
Alex McLean <alex.mclean25@gmail.com>
2024-02-04 14:51:54 -0800

    Splitting up entity fiels

--- packages/core/dynamodb/dynamo.ts
+++ packages/core/dynamodb/dynamo.ts
@@ -1,8 +1,11 @@
 export * as Dynamo from "./dynamo";
 
-import { EntityConfiguration } from "electrodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { EntityConfiguration, Service } from "electrodb";
 import { Table } from "sst/node/table";
+import { OrganizationEntity } from "./entities/orgnization";
+import { PullRequestEntity } from "./entities/pullRequest";
+import { UserEntity } from "./entities/user";
 
 export const Client = new DynamoDBClient({});
 
@@ -10,3 +13,9 @@ export const Configuration: EntityConfiguration = {
   table: Table.db.tableName,
   client: Client,
 };
+
+export const Db = new Service({
+  organization: OrganizationEntity,
+  pullRequest: PullRequestEntity,
+  user: UserEntity,
+});
