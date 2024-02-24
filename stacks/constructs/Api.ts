22cd10f4c1bb1a55cfd94e38595c2c44de37d202
Alex McLean <alex.mclean25@gmail.com>
2024-02-24 10:29:46 -0800

    Removing todo stuff

--- stacks/constructs/Api.ts
+++ stacks/constructs/Api.ts
@@ -36,9 +36,6 @@ export class Api extends Construct {
       },
       routes: {
         "GET /": "packages/functions/src/lambda.handler",
-        // TODO: only for testing, should be removed
-        "GET /list": "packages/functions/src/todo.list",
-        "GET /profile": "packages/functions/src/todo.getUser",
         ...buildPaths("/gh", {
           // Handles incoming webhooks from Github
           "POST /webhook-event": "packages/functions/src/github/events.handler",
