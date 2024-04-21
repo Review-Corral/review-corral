998c8443f29862055b78739ae378339f2d0168bb
Alex McLean <alex.mclean25@gmail.com>
2024-04-21 09:43:36 -0700

    Moving github webhooks to domain

--- packages/domain/github/webhooks/index.ts
+++ packages/domain/github/webhooks/index.ts
@@ -1,10 +1,11 @@
 import * as z from "zod";
-import { fetchOrganizationByAccountId } from "../../dynamodb/fetchers/organizations";
-import { safeFetchRepository } from "../../dynamodb/fetchers/repositories";
-import { getSlackInstallationsForOrganization } from "../../dynamodb/fetchers/slack";
-import { takeFirst } from "../../dynamodb/fetchers/utils";
-import { Logger } from "../../logging";
-import { SlackClient } from "../../slack/SlackClient";
+
+import { SlackClient } from "@core/slack/SlackClient";
+import { fetchOrganizationByAccountId } from "dynamodb/fetchers/organizations";
+import { safeFetchRepository } from "dynamodb/fetchers/repositories";
+import { getSlackInstallationsForOrganization } from "dynamodb/fetchers/slack";
+import { takeFirst } from "dynamodb/fetchers/utils";
+import { Logger } from "logging";
 import { handleIssueCommentEvent } from "./handlers/issueComment";
 import { handlePullRequestEvent } from "./handlers/pullRequest";
 import { handlePullRequestCommentEvent } from "./handlers/pullRequestComment";
