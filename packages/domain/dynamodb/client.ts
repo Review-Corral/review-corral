929d41541923fe82767a07e5fb75512c7127903f
Alex McLean <alex.mclean25@gmail.com>
2024-04-21 09:26:18 -0700

    Adding domian dir

--- packages/domain/dynamodb/client.ts
+++ packages/domain/dynamodb/client.ts
@@ -1,14 +1,14 @@
 export * as Dynamo from "./client";
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { MemberEntity } from "@core/dynamodb/entities/member";
+import { OrganizationEntity } from "@core/dynamodb/entities/organization";
+import { PullRequestEntity } from "@core/dynamodb/entities/pullRequest";
+import { RepositoryEntity } from "@core/dynamodb/entities/repository";
+import { SlackEntity } from "@core/dynamodb/entities/slack";
+import { UserEntity } from "@core/dynamodb/entities/user";
 import { EntityConfiguration, Service } from "electrodb";
 import { Table } from "sst/node/table";
-import { MemberEntity } from "./entities/member";
-import { OrganizationEntity } from "./entities/organization";
-import { PullRequestEntity } from "./entities/pullRequest";
-import { RepositoryEntity } from "./entities/repository";
-import { SlackEntity } from "./entities/slack";
-import { UserEntity } from "./entities/user";
 
 export const Client = new DynamoDBClient({});
 
---
+++ packages/domain/vitest.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    testTimeout: 2000,
+    include: ["**/*.{test,spec,integration}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    globalSetup: ["../../beforeAllTests.ts"],
+  },
+  logLevel: "info",
+  esbuild: {
+    sourcemap: "both",
+  },
+});
