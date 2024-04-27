2c742bf07b2ce4189114d537d92104bb79eda2cf
Alex McLean <alex.mclean25@gmail.com>
2024-04-26 21:16:12 -0700

    Consolidating subscription to organization entity

--- packages/domain/stripe/handlePayment.ts
+++ packages/domain/stripe/handlePayment.ts
@@ -1,43 +1,71 @@
 import { stripeCheckoutCreatedMetadataSchema } from "@core/stripe/types";
-import { insertSubscription } from "@domain/dynamodb/fetchers/subscription";
+import {
+  fetchOrganizationByAccountId,
+  updateOrgStripeSubscription,
+} from "@domain/dynamodb/fetchers/organizations";
 import { Logger } from "@domain/logging";
 import Stripe from "stripe";
 
-const LOGGER = new Logger("stripe.handleSubCreated");
+const LOGGER = new Logger("stripe.handleEvent");
 
-export const handleSubCreated = async (event: Stripe.CheckoutSessionCompletedEvent) => {
-  LOGGER.info(`🚀 Subscription created: ${event.id}`, { event });
+export const handleSubCreated = async (
+  rootEvent: Stripe.CheckoutSessionCompletedEvent,
+) => {
+  LOGGER.info(`🚀 Subscription created: ${rootEvent.id}`, { event: rootEvent });
 
-  const actualEvent = event.data.object;
-  const metadata = stripeCheckoutCreatedMetadataSchema.safeParse(actualEvent.metadata);
+  const event = rootEvent.data.object;
+  const metadata = stripeCheckoutCreatedMetadataSchema.safeParse(event.metadata);
 
   if (!metadata.success) {
     LOGGER.error("Invalid metadata for subCreated event ", { metadata });
     return;
   }
 
-  actualEvent.line_items;
+  const org = await fetchOrganizationByAccountId(metadata.data.orgId);
 
-  if (event.items.data.length !== 1) {
+  if (!org) {
+    LOGGER.error("Organization not found for subCreated event", { metadata });
+    return;
+  }
+
+  const items = event.line_items;
+
+  if (!items) {
+    LOGGER.warn("Recieved subCreated event without line_items", { actualEvent: event });
+    return;
+  }
+
+  if (items.data.length !== 1) {
     LOGGER.warn("Unexpected number of items for subCreated event ", {
-      items: event.items.data,
+      items: rootEvent.data,
     });
     return;
   }
 
-  for (const item of event.items.data) {
-    const createdSubscription = await insertSubscription({
-      orgId: metadata.data.orgId,
-      subId: event.id,
-      priceId: item.id,
-      status: event.status,
-    });
-
-    LOGGER.info(`Subscription created`, {
-      subId: createdSubscription.subId,
-      orgId: createdSubscription.orgId,
-      priceId: createdSubscription.priceId,
-      status: createdSubscription.status,
-    });
+  if (!event.status) {
+    LOGGER.warn("Recieved subCreated event without status", { actualEvent: event });
+    return;
   }
+
+  const priceId = items.data[0].price?.id;
+
+  if (!priceId) {
+    LOGGER.warn("Recieved subCreated event without priceId", {
+      actualEvent: event,
+      priceId,
+    });
+    return;
+  }
+
+  const args = {
+    orgId: org.orgId,
+    customerId: org.customerId,
+    status: event.status,
+    subId: event.id,
+    priceId: priceId,
+  };
+
+  await updateOrgStripeSubscription(args);
+
+  LOGGER.info(`Subscription created`, args);
 };
