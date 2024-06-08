7823dbeba2ad491b55da944f1c26776db2470d11
Alex McLean <alex.mclean25@gmail.com>
2024-06-08 15:23:34 -0700

    specifying pnpm version

--- packages/frontend/src/sst-env.d.ts
+++ packages/frontend/src/sst-env.d.ts
@@ -7,7 +7,6 @@ interface ImportMetaEnv {
   readonly VITE_SLACK_BOT_ID: string
   readonly VITE_SLACK_CLIENT_SECRET: string
   readonly VITE_SLACK_AUTH_URL: string
-  readonly VITE_LOCAL: string
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv
