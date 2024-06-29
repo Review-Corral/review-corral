091a3a564eb3741e92d9cdb2e2247f381dc1bf55
Alex McLean <alex.mclean25@gmail.com>
2024-06-29 14:51:50 -0700

    Removing old frontend construct from FrontendStack

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -22,33 +22,7 @@ export function FrontendStack({ stack, app }: StackContext) {
   use(StorageStack);
   const { authUrl } = use(AuthStack);
 
-  // Define our React app
-  const site = new StaticSite(stack, "ReactSite", {
-    path: "packages/frontend",
-    buildCommand: "pnpm run build",
-    buildOutput: "dist",
-    customDomain: app.local
-      ? undefined
-      : {
-          domainName: getFrontendUrl(app),
-          hostedZone: HOSTED_ZONE,
-        },
-    // Pass in our environment variables
-    environment: {
-      VITE_API_URL: api.customDomainUrl ?? api.url,
-      VITE_REGION: app.region,
-      VITE_AUTH_URL: authUrl,
-      VITE_STRIPE_PRICE_ID:
-        stack.stage === "prod"
-          ? "price_1P8CmKBqa9UplzHebShipTnE"
-          : "price_1P9FpDBqa9UplzHeeJ57VHoc",
-      ...slackEnvVars,
-      ...(app.local ? { VITE_LOCAL: "true" } : {}),
-    },
-  });
-
-  // Define our React app
-  const _reactSite = new NextjsSite(stack, "NextJsSite", {
+  const site = new NextjsSite(stack, "NextJsSite", {
     path: "packages/web",
     buildCommand: "pnpm run build",
     customDomain: app.local
