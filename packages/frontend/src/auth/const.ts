1e7a4e9c9192410e743dfb843186e0de9b68f462
Alex McLean <alex.mclean25@gmail.com>
2024-07-19 23:03:21 -0700

    Removing frontend dir

--- packages/frontend/src/auth/const.ts
+++
@@ -1,1 +0,0 @@
-export const auth_access_token_key = "sst_auth_access_token";
--- packages/frontend/src/organization/payments/success.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { SharedPaymentsView } from "./shared";
-
-interface PaymentsSuccessProps {}
-
-export const PaymentsSuccess: FC<PaymentsSuccessProps> = () => {
-  return <SharedPaymentsView>Payment Success!</SharedPaymentsView>;
-};
