5c01fa7d219d0b62db2de5b29427c66ad36273d3
Alex McLean <alex.mclean25@gmail.com>
2024-11-16 15:05:27 -0800

    Large cleanup of updating the main thread in slack

--- packages/domain/slack/SlackClient.ts
+++ packages/domain/slack/SlackClient.ts
@@ -1,6 +1,7 @@
+import { PullRequestItem } from "@core/dynamodb/entities/types";
+import { convertPrEventToBaseProps } from "@domain/github/webhooks/handlers/utils";
 import {
   PullRequestClosedEvent,
-  PullRequestEvent,
   PullRequestOpenedEvent,
   PullRequestReadyForReviewEvent,
   PullRequestReview,
@@ -9,15 +10,19 @@ import {
   ChatPostMessageArguments,
   ChatPostMessageResponse,
   ChatUpdateArguments,
-  ContextBlock,
-  ImageElement,
-  MessageAttachment,
-  SectionBlock,
   WebClient,
 } from "@slack/web-api";
 import slackifyMarkdown from "slackify-markdown";
 import { Logger } from "../logging";
-import { PullRequestItem } from "@core/dynamodb/entities/types";
+import {
+  MainMessageArgs,
+  RequiredApprovalsQueryPayloadArg,
+  buidMainMessageAttachements,
+  getBaseChatUpdateArguments,
+  getConvertedToDraftAttachment,
+  getMergedAttachment,
+  getPrClosedAttatchment,
+} from "./mainMessage";
 
 export type PullRequestEventOpenedOrReadyForReview =
   | PullRequestOpenedEvent
@@ -40,6 +45,12 @@ export type BasePullRequestProperties = {
     base: {
       ref: string;
     };
+    merged: boolean;
+    draft: boolean;
+    /**
+     * If this is NOT null but merged is false, then it's a closed PR
+     */
+    closed_at: string | null;
   };
   repository: {
     full_name: string;
@@ -59,6 +70,7 @@ export class SlackClient {
     this.client = new WebClient(this.slackToken);
   }
 
+  // TODO: Ideally this is private
   async postMessage({
     message,
     threadTs,
@@ -87,95 +99,31 @@ export class SlackClient {
     }
   }
 
-  async postPrMerged(
-    body: PullRequestClosedEvent,
-    threadTs: string,
-    slackUsername: string,
-  ) {
+  async postPrMerged(args: MainMessageArgs<BasePullRequestProperties>) {
     await this.postMessage({
       message: {
-        text: `Pull request merged by ${slackUsername}`,
-        attachments: [this.getMergedAttachment()],
+        text: `Pull request merged by ${args.slackUsername}`,
+        attachments: [getMergedAttachment()],
       },
-      threadTs,
+      threadTs: args.threadTs,
     });
 
-    if (threadTs) {
-      console.debug(
-        `Going to update message ts: ${threadTs} for channel ${this.channelId}`,
-      );
-      const payload: ChatUpdateArguments = {
-        channel: this.channelId,
-        ts: threadTs,
-        token: this.slackToken,
-        text: await this.getPrOpenedMessage(slackUsername),
-        attachments: [
-          await this.getPrOpenedBaseAttachment(body, slackUsername),
-          this.getMergedAttachment(),
-        ],
-      };
-
-      LOGGER.debug("Payload: ", payload);
-
-      try {
-        await this.client.chat.update(payload);
-        LOGGER.debug("Succesfully updated message with merged status");
-      } catch (error) {
-        LOGGER.error("Got error updating thread with merged status: ", error);
-      }
-    }
+    await this.updateMainMessage(args, "pr-merged");
   }
 
-  async postConvertedToDraft(
-    body: PullRequestEvent,
-    threadTs: string,
-    slackUsername: string,
-    pullRequestItem: PullRequestItem,
-  ) {
+  async postConvertedToDraft(args: MainMessageArgs<BasePullRequestProperties>) {
     await this.postMessage({
       message: {
         text: "Pull request converted to draft",
-        attachments: [this.getConvertedToDraftAttachment()],
+        attachments: [getConvertedToDraftAttachment()],
       },
-      threadTs,
+      threadTs: args.threadTs,
     });
 
-    LOGGER.debug(
-      `Going to update message ts: ${threadTs} for channel ${this.channelId}`,
-    );
-
-    const defaultPayload = await this.getBaseChatUpdateArguments({
-      body,
-      threadTs,
-      slackUsername,
-      pullRequestItem,
-    });
-
-    try {
-      await this.client.chat.update({
-        ...defaultPayload,
-        attachments: [
-          ...(defaultPayload.attachments || []),
-          this.getConvertedToDraftAttachment("Pull request marked as draft"),
-        ],
-      });
-      LOGGER.debug("Succesfully updated message with converted to draft status");
-    } catch (error) {
-      LOGGER.error("Got error updating thread with converted to draft status: ", error);
-    }
+    await this.updateMainMessage(args, "pr-converted-to-draft");
   }
 
-  async postReadyForReview({
-    body,
-    threadTs,
-    slackUsername,
-    pullRequestItem,
-  }: {
-    body: PullRequestEvent;
-    threadTs: string;
-    slackUsername: string;
-    pullRequestItem: PullRequestItem | null;
-  }) {
+  async postReadyForReview(args: MainMessageArgs<BasePullRequestProperties>) {
     await this.postMessage({
       message: {
         text: "Draft status removed",
@@ -194,85 +142,16 @@ export class SlackClient {
           },
         ],
       },
-      threadTs,
+      threadTs: args.threadTs,
     });
 
-    LOGGER.debug(
-      `Going to update message ts: ${threadTs} for channel ${this.channelId}`,
-    );
-
-    const defaultPayload = await this.getBaseChatUpdateArguments({
-      body,
-      threadTs,
-      slackUsername,
-      pullRequestItem,
-    });
-
-    try {
-      await this.client.chat.update(defaultPayload);
-      LOGGER.debug("Succesfully updated message with ready for review status");
-    } catch (error) {
-      LOGGER.error("Got error updating thread with ready for review status: ", error);
-    }
+    await this.updateMainMessage(args, "pr-ready");
   }
 
-  async updateMainPrMessage({
-    body,
-    threadTs,
-    slackUsername,
-    pullRequestItem,
-  }: {
-    body: BasePullRequestProperties;
-    threadTs: string;
-    slackUsername: string;
-    pullRequestItem: PullRequestItem;
-  }) {
-    LOGGER.info(
-      `Going to update message ts: ${threadTs} for channel ${this.channelId}`,
-    );
-
-    const defaultPayload = await this.getBaseChatUpdateArguments({
-      body,
-      threadTs,
-      slackUsername,
-      pullRequestItem,
-    });
-
-    try {
-      await this.client.chat.update(defaultPayload);
-      LOGGER.info("Succesfully updated message");
-    } catch (error) {
-      LOGGER.error("Got error updating message", { error });
-    }
+  // TODO: where this used?
+  async updateMainPrMessage(args: MainMessageArgs<BasePullRequestProperties>) {
+    await this.updateMainMessage(args, "update");
   }
-
-  getBaseChatUpdateArguments = async ({
-    body,
-    threadTs,
-    slackUsername,
-    pullRequestItem,
-  }: {
-    body: BasePullRequestProperties;
-    threadTs: string;
-    slackUsername: string;
-    pullRequestItem: PullRequestItem | null;
-  }): Promise<ChatUpdateArguments> => ({
-    channel: this.channelId,
-    ts: threadTs,
-    token: this.slackToken,
-    text: await this.getPrOpenedMessage(slackUsername),
-    attachments: [
-      this.getPrOpenedBaseAttachment(body, slackUsername),
-      ...(pullRequestItem?.requiredApprovals && pullRequestItem.approvalCount
-        ? [
-            this.getRequiredApprovalsAttatchment({
-              requiredApprovals: pullRequestItem.requiredApprovals,
-              approvalCount: pullRequestItem.approvalCount,
-            }),
-          ]
-        : []),
-    ],
-  });
 
   async postComment({
     commentBody,
@@ -301,45 +180,43 @@ export class SlackClient {
     });
   }
 
-  async postPrClosed(
-    body: PullRequestClosedEvent,
-    threadTs: string,
-    slackUsername: string,
-    pullRequestItem: PullRequestItem,
-  ) {
+  async getBaseMessageArgsWithToken(
+    args: MainMessageArgs<BasePullRequestProperties>,
+  ): Promise<ChatUpdateArguments> {
+    return {
+      token: this.slackToken,
+      channel: this.channelId,
+      ...(await getBaseChatUpdateArguments(args)),
+    };
+  }
+
+  async postPrClosed(args: MainMessageArgs<PullRequestClosedEvent>) {
     await this.postMessage({
       message: {
-        attachments: [this.getPrClosedAttatchment(slackUsername)],
+        attachments: [getPrClosedAttatchment(args.slackUsername)],
       },
-      threadTs,
+      threadTs: args.threadTs,
     });
 
-    const defaultPayload = await this.getBaseChatUpdateArguments({
-      body,
-      threadTs,
-      slackUsername,
-      pullRequestItem,
-    });
+    await this.updateMainMessage(args, "pr-closed");
+  }
 
+  async updateMainMessage(
+    args: MainMessageArgs<BasePullRequestProperties>,
+    eventName: string,
+  ) {
     try {
-      await this.client.chat.update({
-        ...defaultPayload,
-        attachments: [
-          ...(defaultPayload.attachments || []),
-          this.getPrClosedAttatchment(slackUsername),
-        ],
-      });
-      console.debug("Succesfully updated message with closed pr status");
+      await this.client.chat.update(await this.getBaseMessageArgsWithToken(args));
+      LOGGER.debug(`Succesfully updated main message for ${eventName}`);
     } catch (error) {
-      console.error("Got error updating thread with closed pr status: ", error);
+      LOGGER.error(`Got error updating main message for ${eventName}: `, error);
     }
   }
 
-  async postReviewRequested(
-    _body: PullRequestClosedEvent,
-    threadTs: string,
-    slackUsername: string,
-  ) {
+  async postReviewRequested({
+    threadTs,
+    slackUsername,
+  }: MainMessageArgs<PullRequestClosedEvent>) {
     return await this.postMessage({
       message: {
         attachments: [
@@ -361,13 +238,15 @@ export class SlackClient {
     });
   }
 
-  async postReview(
-    _prId: number,
-    review: PullRequestReview,
-    _login: string,
-    threadTs: string,
-    slackUsername: string,
-  ) {
+  async postReview({
+    review,
+    threadTs,
+    slackUsername,
+  }: {
+    review: PullRequestReview;
+    threadTs: string;
+    slackUsername: string;
+  }) {
     const getReviewText = (review: PullRequestReview) => {
       switch (review.state) {
         case "approved": {
@@ -406,188 +285,32 @@ export class SlackClient {
     return `Pull request opened by ${slackUsername}`;
   }
 
-  private getRequiredApprovalsAttatchment({
+  async postPrReady({
+    body,
+    pullRequestItem,
+    slackUsername,
     requiredApprovals,
-    approvalCount,
   }: {
-    requiredApprovals: number;
-    approvalCount: number;
-  }): MessageAttachment {
-    if (approvalCount >= requiredApprovals) {
-      return {
-        color: "#03BB00",
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `:bust_in_silhouette: ${approvalCount}/${requiredApprovals} approvals required`,
-            },
-          },
-        ],
-      };
-    }
-
-    return {
-      color: "#D9CD27",
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `:bust_in_silhouette: ${approvalCount}/${requiredApprovals} approvals required`,
-          },
-        },
-      ],
-    };
-  }
-
-  private getPrOpenedBaseAttachment(
-    body: BasePullRequestProperties,
-    slackUsername: string,
-  ): MessageAttachment {
-    return {
-      blocks: [
-        {
-          type: "header",
-          text: {
-            type: "plain_text",
-            text: `${body.pull_request.title} #${body.pull_request.number}`,
-          },
-        },
-
-        {
-          type: "actions",
-          elements: [
-            {
-              type: "button",
-              text: {
-                type: "plain_text",
-                text: "View",
-              },
-              url: body.pull_request.html_url,
-            },
-          ],
-        },
-        ...(body.pull_request?.body
-          ? [
-              {
-                type: "divider",
-              },
-              {
-                type: "section",
-                text: {
-                  type: "mrkdwn",
-                  text: slackifyMarkdown(body.pull_request.body),
-                },
-              } as SectionBlock,
-            ]
-          : []),
-        {
-          type: "context",
-          elements: [
-            {
-              type: "image",
-              image_url: body.repository.owner.avatar_url,
-              alt_text: "repo owner url",
-            },
-            {
-              type: "mrkdwn",
-              text: `${body.repository.full_name}`,
-            },
-            {
-              type: "image",
-              image_url: body.pull_request.user.avatar_url,
-              alt_text: "user url",
-            },
-            {
-              type: "mrkdwn",
-              text: `${slackUsername}`,
-            },
-            {
-              type: "image",
-              // For whatever reason, Slack won't use the image at
-              // https://reviewcorral.com/plus-minus-diff-icon-alt.png
-              // it seems likely it's due to the domain not being whitelisted by Slack.
-              // Using this as a fallback for now.
-              image_url: "https://cdn-icons-png.flaticon.com/512/9296/9296948.png",
-              alt_text: "plus-minus-icon",
-            } as ImageElement,
-            {
-              type: "mrkdwn",
-              text: `+${body.pull_request.additions}-${body.pull_request.deletions}`,
-            },
-            {
-              type: "mrkdwn",
-              text: `:dart: ${body.pull_request.base.ref}`,
-            },
-          ],
-        } as ContextBlock,
-      ],
-    };
-  }
-
-  async postPrReady(
-    body: PullRequestEventOpenedOrReadyForReview,
-    slackUsername: string,
-  ): Promise<ChatPostMessageResponse | undefined> {
+    body: PullRequestEventOpenedOrReadyForReview;
+    pullRequestItem: PullRequestItem | null;
+    slackUsername: string;
+    requiredApprovals: RequiredApprovalsQueryPayloadArg;
+  }): Promise<ChatPostMessageResponse | undefined> {
     try {
       return this.postMessage({
         message: {
           text: await this.getPrOpenedMessage(slackUsername),
-          attachments: [this.getPrOpenedBaseAttachment(body, slackUsername)],
+          attachments: buidMainMessageAttachements({
+            body: convertPrEventToBaseProps(body),
+            slackUsername,
+            pullRequestItem,
+            requiredApprovals,
+          }),
         },
         threadTs: undefined,
       });
     } catch (error) {
       console.error("Got error posting to channel: ", error);
     }
-  }
-
-  private getMergedAttachment(): MessageAttachment {
-    return {
-      color: "#8839FB",
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: ":large_purple_circle: Pull request merged",
-          },
-        },
-      ],
-    };
-  }
-
-  private getConvertedToDraftAttachment(
-    text = "Pull request converted back to draft",
-  ): MessageAttachment {
-    return {
-      color: "#D9CD27",
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `:construction: ${text}`,
-          },
-        },
-      ],
-    };
-  }
-
-  private getPrClosedAttatchment(slackUsername: string): MessageAttachment {
-    return {
-      color: "#FB0909",
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `:red_circle: Pull request closed by ${slackUsername}`,
-          },
-        },
-      ],
-    };
   }
 }
