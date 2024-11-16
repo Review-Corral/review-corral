1fa26d7d75377a0bf18df7a61de6e422913c413c
Alex McLean <alex.mclean25@gmail.com>
2024-11-16 09:19:02 -0800

    Fixing typechecks

--- packages/domain/github/webhooks/handlers/pullRequestComment.ts
+++ packages/domain/github/webhooks/handlers/pullRequestComment.ts
@@ -1,7 +1,8 @@
+import { fetchPrItem } from "@domain/dynamodb/fetchers/pullRequests";
 import { PullRequestReviewCommentCreatedEvent } from "@octokit/webhooks-types";
 import { Logger } from "../../../logging";
 import { GithubWebhookEventHander } from "../types";
-import { getSlackUserName, getThreadTs } from "./shared";
+import { getSlackUserName } from "./shared";
 
 const LOGGER = new Logger("core.github.webhooks.handlers.pullRequest");
 
@@ -9,12 +10,12 @@ export const handlePullRequestCommentEvent: GithubWebhookEventHander<
   PullRequestReviewCommentCreatedEvent
 > = async ({ event, slackClient, ...args }) => {
   if (event.action === "created" && event.comment.user.type === "User") {
-    const threadTs = await getThreadTs({
-      prId: event.pull_request.id,
+    const pullRequestItem = await fetchPrItem({
+      pullRequestId: event.pull_request.id,
       repoId: event.repository.id,
     });
 
-    if (!threadTs) {
+    if (!pullRequestItem?.threadTs) {
       // write error log
       LOGGER.warn("Got a comment event but couldn't find the thread", {
         action: event.action,
@@ -28,7 +29,7 @@ export const handlePullRequestCommentEvent: GithubWebhookEventHander<
       prId: event.pull_request.id,
       commentBody: event.comment.body,
       commentUrl: event.comment.html_url,
-      threadTs: threadTs,
+      threadTs: pullRequestItem.threadTs,
       slackUsername: await getSlackUserName(event.sender.login, args),
     });
     return;
