9f1b26ed8e3bb86e3d73e198a4e4381818569d10
Alex McLean <alex.mclean25@gmail.com>
2024-12-20 15:46:15 -0800

    Better footer location

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./Navbar";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {/* <div className="min-h-full min-w-[900px] overflow-x-auto"> */}
-        <main className="">{children}</main>
-        {/* </div> */}
+      <body className={`${inter.className} bg-gray-50`}>
+        <div className="h-[100dvh] flex flex-col">
+          <Navbar />
+          <div className="flex-grow flex flex-col">
+            <main className="flex-grow">{children}</main>
+            <footer className="container mx-auto px-4 py-8 text-center text-gray-600">
+              <div className="flex justify-center items-center gap-4">
+                <p>
+                  &copy; {new Date().getFullYear()} Review Corral. All rights reserved.
+                </p>
+                <p className="text-gray-300"> | </p>
+                <p>
+                  <Link href="/privacy">Privacy Policy</Link>
+                </p>
+              </div>
+            </footer>
+          </div>
+        </div>
       </body>
     </html>
   );
