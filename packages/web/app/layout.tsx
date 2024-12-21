5801928f5a1675d10251aecaa432571fbdca4fbf
Alex McLean <alex.mclean25@gmail.com>
2024-12-20 16:19:15 -0800

    Got layout working better for landing page -> app

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "./Navbar";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,25 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50`}>
-        <div className="h-[100dvh] flex flex-col">
-          <Navbar />
-          <div className="flex-grow flex flex-col">
-            <main className="flex-grow">{children}</main>
-            <footer className="container mx-auto px-4 py-8 text-center text-gray-600">
-              <div className="flex justify-center items-center gap-4">
-                <p>
-                  &copy; {new Date().getFullYear()} Review Corral. All rights reserved.
-                </p>
-                <p className="text-gray-300"> | </p>
-                <p>
-                  <Link href="/privacy">Privacy Policy</Link>
-                </p>
-              </div>
-            </footer>
-          </div>
-        </div>
-      </body>
+      <body className={`${inter.className} bg-gray-50`}>{children}</body>
     </html>
   );
 }
