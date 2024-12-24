f7a191078aeb8197843dc78d6c621437e199b0d0
Alex McLean <alex.mclean25@gmail.com>
2024-12-24 10:35:36 -0800

    Trying simpler option

--- infra/auth.ts
+++ infra/auth.ts
@@ -6,7 +6,8 @@ const ghClientSecret = new sst.Secret("GithubClientSecret");
 export const auth = new sst.aws.Auth("Auth", {
   authorizer: {
     link: [table, ghClientId, ghClientSecret],
-    handler: "./packages/functions/src/auth/handler.handler",
+    handler: "./infra/src/authHandler.handler",
     url: true,
   },
+  forceUpgrade: "v2",
 });
