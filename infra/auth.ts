4aaf4a515631c25666038d75b030270d166481d6
Alex McLean <alex.mclean25@gmail.com>
2024-12-30 21:31:29 -0800

    Rename authserver

--- infra/auth.ts
+++ infra/auth.ts
@@ -6,7 +6,7 @@ const ghClientSecret = new sst.Secret("GithubClientSecret");
 export const auth = new sst.aws.Auth("Auth", {
   authorizer: {
     link: [table, ghClientId, ghClientSecret],
-    handler: "./packages/functions/src/auth/handler.handler",
+    handler: "./packages/functions/src/auth/authServer.handler",
     url: true,
   },
   forceUpgrade: "v2",
