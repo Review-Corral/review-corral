c4bb8961315860ff684d7daf6823072f4f7f5fe1
Alex McLean <alex.mclean25@gmail.com>
2025-01-12 14:15:56 -0800

    Adding hono for auth

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -1,10 +1,8 @@
+import { HostedZone } from "aws-cdk-lib/aws-route53";
 import { App, NextjsSite, StackContext, use } from "sst/constructs";
-
-import { AuthStack } from "./AuthStack";
 import { MainStack } from "./MainStack";
 import { StorageStack } from "./StorageStack";
 import { HOSTED_ZONE } from "./constructs/Api";
-import { HostedZone } from "aws-cdk-lib/aws-route53";
 
 export const getFrontendUrl = ({ local, stage }: App) => {
   if (local) return "http://localhost:3000";
@@ -18,7 +16,6 @@ export function FrontendStack({ stack, app }: StackContext) {
   const { slackEnvVars, api } = use(MainStack);
   // Just here to try and fix typing
   const { table } = use(StorageStack);
-  const { authUrl } = use(AuthStack);
 
   const frontendUrl = getFrontendUrl(app);
 
@@ -41,7 +38,6 @@ export function FrontendStack({ stack, app }: StackContext) {
     environment: {
       NEXT_PUBLIC_API_URL: api.api.customDomainUrl ?? api.api.url,
       NEXT_PUBLIC_REGION: app.region,
-      NEXT_PUBLIC_AUTH_URL: authUrl,
       NEXT_PUBLIC_STRIPE_PRICE_ID:
         stack.stage === "prod"
           ? "price_1P8CmKBqa9UplzHebShipTnE"
