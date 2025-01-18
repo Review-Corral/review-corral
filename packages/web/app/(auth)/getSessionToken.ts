33912692fbb7593ea51fd0487c55d85d88a6f9cc
Alex McLean <alex.mclean25@gmail.com>
2025-01-18 15:31:26 -0800

    Auth working with non-http-only token

--- packages/web/app/(auth)/getSessionToken.ts
+++ packages/web/app/(auth)/getSessionToken.ts
@@ -7,8 +7,10 @@ export const getSessionToken = (): string => {
   const token = Cookies.get(AuthAccessTokenKey);
 
   if (!token) {
+    console.log("No access token found");
     throw new Error("User not logged in");
   }
 
+  console.log("Found access token");
   return token;
 };
