7cf67f8bd24d1dadf406448bdea087b5e645d782
Alex McLean <alex.mclean25@gmail.com>
2025-01-18 15:56:01 -0800

    Setting up useUSer to not use SST auth

--- packages/functions/src/utils/useUser.ts
+++ packages/functions/src/utils/useUser.ts
@@ -1,26 +1,44 @@
 import { User } from "@core/dynamodb/entities/types";
+import { assertVarExists } from "@core/utils/assert";
 import { fetchUserById } from "@domain/dynamodb/fetchers/users";
-import { useSession } from "sst/node/auth";
+import { Logger } from "@domain/logging";
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { JwtPayload, verify } from "jsonwebtoken";
 
-export const useUser = async (): Promise<{
+export const useUser = async (
+  event: APIGatewayProxyEventV2,
+  logger: Logger,
+): Promise<{
   user: User | null;
   error: string | null;
 }> => {
-  const session = useSession();
+  const authToken = event.headers.authorization;
 
-  console.log("Session2", { session });
-
-  if (session.type !== "user") {
-    return { user: null, error: "User not set in session" };
+  if (!authToken) {
+    logger.warn("No auth token found in request", { event });
+    return { user: null, error: "No auth token found" };
   }
 
-  const userId = session.properties.userId;
+  let verifiedJwt: JwtPayload | string | null = null;
 
-  console.log("Searching for user with UserId: ", userId);
+  try {
+    verifiedJwt = verify(authToken, assertVarExists<string>("JWT_SECRET"));
+  } catch (error) {
+    logger.error("Error verifying JWT", { error });
+    return { user: null, error: "Error verifying JWT" };
+  }
+
+  // Shouldn't happen
+  if (typeof verifiedJwt !== "object" || !verifiedJwt) {
+    logger.error("No verified JWT found", { verifiedJwt });
+    return { user: null, error: "No verified JWT found" };
+  }
+
+  const { userId } = verifiedJwt as { userId: number };
 
   const user = await fetchUserById(userId);
 
-  console.log("Returned user", user);
+  logger.info("Returned user", user);
 
   if (!user) return { user: null, error: "No user found in database" };
 
