f544826739768bf01d177f60b7fea4d00af4cb60
Alex McLean <alex.mclean25@gmail.com>
2025-01-19 20:28:58 -0800

    finished overriding

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,39 +1,51 @@
-const table = new sst.aws.Dynamo("MainTable", {
-  fields: {
-    pk: "string",
-    sk: "string",
-    lsi1sk: "string",
-    gsi1pk: "string",
-    gsi1sk: "string",
-    gsi2pk: "string",
-    gsi2sk: "string",
-    gsi3pk: "string",
-    gsi3sk: "string",
-  },
-  primaryIndex: {
-    hashKey: "pk",
-    rangeKey: "sk",
-  },
-  globalIndexes: {
-    gsi1: {
-      hashKey: "gsi1pk",
-      rangeKey: "gsi1sk",
+import { getStageLookups } from "./utils/lookups";
+
+const table = new sst.aws.Dynamo(
+  "MainTable",
+  {
+    transform: {
+      table: (args, opts) => {
+        args.name = getStageLookups($app.stage).table.name;
+        opts.import = getStageLookups($app.stage).table.import;
+      },
     },
-    gsi2: {
-      hashKey: "gsi2pk",
-      rangeKey: "gsi2sk",
+    fields: {
+      pk: "string",
+      sk: "string",
+      lsi1sk: "string",
+      gsi1pk: "string",
+      gsi1sk: "string",
+      gsi2pk: "string",
+      gsi2sk: "string",
+      gsi3pk: "string",
+      gsi3sk: "string",
     },
-    gsi3: {
-      hashKey: "gsi3pk",
-      rangeKey: "gsi3sk",
+    primaryIndex: {
+      hashKey: "pk",
+      rangeKey: "sk",
     },
+    globalIndexes: {
+      gsi1: {
+        hashKey: "gsi1pk",
+        rangeKey: "gsi1sk",
+      },
+      gsi2: {
+        hashKey: "gsi2pk",
+        rangeKey: "gsi2sk",
+      },
+      gsi3: {
+        hashKey: "gsi3pk",
+        rangeKey: "gsi3sk",
+      },
+    },
+    localIndexes: {
+      lsi1: {
+        rangeKey: "lsi1sk",
+      },
+    },
+    ttl: "expireAt",
   },
-  localIndexes: {
-    lsi1: {
-      rangeKey: "lsi1sk",
-    },
-  },
-  ttl: "expireAt",
-});
+  {},
+);
 
 export { table };
