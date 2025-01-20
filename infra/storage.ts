9c9cb29631b6cf5c8c1f9df7191d5c30b1db4464
Alex McLean <alex.mclean25@gmail.com>
2025-01-19 16:04:15 -0800

    Adding infra components

---
+++ infra/storage.ts
@@ -0,0 +1,39 @@
+const table = new sst.aws.Dynamo("MyTable", {
+  fields: {
+    pk: "string",
+    sk: "string",
+    lsi1sk: "string",
+    gsi1pk: "string",
+    gsi1sk: "string",
+    gsi2pk: "string",
+    gsi2sk: "string",
+    gsi3pk: "string",
+    gsi3sk: "string",
+  },
+  primaryIndex: {
+    hashKey: "pk",
+    rangeKey: "sk",
+  },
+  globalIndexes: {
+    gsi1: {
+      hashKey: "gsi1pk",
+      rangeKey: "gsi1sk",
+    },
+    gsi2: {
+      hashKey: "gsi2pk",
+      rangeKey: "gsi2sk",
+    },
+    gsi3: {
+      hashKey: "gsi3pk",
+      rangeKey: "gsi3sk",
+    },
+  },
+  localIndexes: {
+    lsi1: {
+      rangeKey: "lsi1sk",
+    },
+  },
+  ttl: "expireAt",
+});
+
+export { table };
--- sst.config.ts
+++ sst.config.ts
@@ -7,7 +7,29 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        aws: {
+          region: "us-east-1",
+          profile: "rc",
+        },
+      },
     };
   },
-  async run() {},
+  async run() {
+    $transform(sst.aws.Function, (args, _opts) => {
+      // Set the default if it's not set by the component
+      if (args.runtime === undefined) {
+        args.runtime = "nodejs18.x";
+        args.architecture = "arm64";
+        args.logging =
+          $app.stage === "prod"
+            ? {
+                retention: "6 months",
+              }
+            : {
+                retention: "1 week",
+              };
+      }
+    });
+  },
 });
