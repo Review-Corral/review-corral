2ae343fcf4901bd629bdf04c8aeef115f94ad097
Alex McLean <alex.mclean25@gmail.com>
2025-01-30 20:32:03 -0800

    updating sst

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -57,14 +57,6 @@ declare module "sst" {
       "type": "sst.sst.Secret"
       "value": string
     }
-    "api": {
-      "type": "sst.aws.ApiGatewayV2"
-      "url": string
-    }
-    "frontend": {
-      "type": "sst.aws.Nextjs"
-      "url": string
-    }
   }
 }
 /// <reference path="sst-env.d.ts" />
