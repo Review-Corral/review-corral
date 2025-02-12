a31cf238e2cca064ef41591e9bcc958abc2f7bbd
Alex <alex.mclean25@gmail.com>
2025-02-11 22:06:20 -0800

    Better footer formatting

--- packages/web/app/(landing)/layout.tsx
+++ packages/web/app/(landing)/layout.tsx
@@ -7,12 +7,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Navbar />
       <div className="flex-grow flex flex-col">
         <main className="flex-grow">{children}</main>
-        <footer className="container mx-auto px-4 py-8 text-center text-gray-600">
+        <footer className="text-xs md:text-sm container mx-auto px-4 py-8 text-center text-gray-600">
           <div className="flex justify-center items-center gap-4">
             <p>&copy; {new Date().getFullYear()} Review Corral. All rights reserved.</p>
             <p className="text-gray-300"> | </p>
             <p>
-              <Link href="/privacy">Privacy Policy</Link>
+              <Link href="/privacy" className="underline">
+                Privacy Policy
+              </Link>
             </p>
           </div>
         </footer>
