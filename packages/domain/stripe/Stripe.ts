57fb81a25c4a23e377a8f1bc74a0fd10b8ee06ef
Alex McLean <alex.mclean25@gmail.com>
2025-03-13 21:53:49 -0700

    Upgrading packages

--- packages/domain/stripe/Stripe.ts
+++ packages/domain/stripe/Stripe.ts
@@ -2,5 +2,5 @@ import { Resource } from "sst";
 import { Stripe } from "stripe";
 
 export const StripeClient = new Stripe(Resource.STRIPE_SECRET_KEY.value, {
-  apiVersion: "2024-04-10",
+  apiVersion: "2025-02-24.acacia",
 });
