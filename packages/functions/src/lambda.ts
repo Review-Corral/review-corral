bddd7b381ff717fd658e49fb2a75bfb8c83dc728
Alex McLean <alex.mclean25@gmail.com>
2025-04-02 21:27:32 -0700

    Deleting unused files

--- packages/functions/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from "@src/apiHandler";
-
-export const handler = ApiHandler(async (_evt) => {
-  return {
-    statusCode: 200,
-    body: `Hello World. The time is ${new Date().toISOString()}`,
-  };
-});
