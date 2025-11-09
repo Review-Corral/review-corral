609e92d3eddbe9831ea35d646e1c12691834993c
Alex McLean <alex.mclean25@gmail.com>
2025-11-08 21:39:04 -0800

    Adding nitro for deploying TSS

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,10 +1,21 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), tanstackStart(), viteReact(), viteTsConfigPaths()],
+  plugins: [
+    tailwindcss(),
+    tanstackStart(),
+    nitro({
+      config: {
+        preset: "aws-lambda",
+      },
+    }),
+    viteReact(),
+    viteTsConfigPaths(),
+  ],
 });
