13d5fd4297333275810cce581361724977d5ba96
Alex McLean <alex.mclean25@gmail.com>
2025-11-11 13:11:17 -0800

    Better abstraction fo slack install URL

--- packages/web/src/components/dashboard/tabs/slack/SetupSlackButton.tsx
+++ packages/web/src/components/dashboard/tabs/slack/SetupSlackButton.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@components/shadcn/button";
 import React from "react";
+import { getSlackInstallUrl } from "./getSlackInstallUrl";
 
 interface SlackButtonProps {
   organizationId: number;
@@ -10,30 +11,11 @@ const SlackButton: React.FC<SlackButtonProps> = ({
   organizationId,
   buttonText = "Connect to Slack",
 }) => {
-  const slackBotId = import.meta.env.VITE_SLACK_BOT_ID;
-  if (!slackBotId) {
-    throw Error("VITE_SLACK_BOT_ID not set");
-  }
-
-  const apiUrl = import.meta.env.VITE_API_URL;
-  if (!apiUrl) {
-    throw Error("VITE_API_URL not set");
-  }
-
-  const slackAuthUrl = `${apiUrl}/slack/oauth`;
-
-  const searchParams = new URLSearchParams({
-    state: organizationId.toString(),
-    redirect_uri: slackAuthUrl,
-    client_id: slackBotId,
-    scope: "channels:join,chat:write,chat:write.public,im:write,users:read",
-    user_scope: "",
-  });
-
   // TODO: invalidate queries when launching this
+  // NOTE: we do invalidate on the success redirect route
   return (
     <div>
-      <a href={`https://slack.com/oauth/v2/authorize?${searchParams.toString()}`}>
+      <a href={getSlackInstallUrl(organizationId)}>
         <Button variant="outline">{buttonText}</Button>
       </a>
     </div>
