9fa5d44042301d41250c549f79c8066f2ebe4351
Alex McLean <alex.mclean25@gmail.com>
2025-11-11 14:17:28 -0800

    Better scopes checking for Slack

--- packages/core/slack/const.ts
+++ packages/core/slack/const.ts
@@ -1,2 +1,22 @@
 export const LATEST_SLACK_SCOPES =
-  "channels:join,chat:write,chat:write.public,im:write,users:read";
+  "channels:join,chat:write,chat:write.public,im:write,users:read,commands,channels:history";
+
+// For some reason, Slack will give us these instead of the above which we put in the request
+const expectedScopes = [
+  "chat:write",
+  "channels:history",
+  // "groups:history", This comes back for some reason even though we don't request it
+  "commands",
+  "users:read",
+  "chat:write.public",
+  "channels:join",
+  "im:write",
+];
+
+/**
+ * Checks if we have the required scopes installed
+ */
+export const haveRequiredScopes = (scopes: string): boolean => {
+  const split = scopes.split(",");
+  return expectedScopes.every((scope) => split.includes(scope));
+};
