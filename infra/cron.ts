62be620451d6d73d98c0444ac9fa47c5ba1799f7
Alex McLean <alex.mclean25@gmail.com>
2026-01-13 18:09:02 -0800

    Adding cron for PR reminders

---
+++ infra/cron.ts
@@ -0,0 +1,13 @@
+import { table } from "./storage";
+
+// PR Reminder CRON job
+// Runs every weekday (Mon-Fri) at 9am ET (14:00 UTC)
+new sst.aws.Cron("PrReminderCron", {
+  schedule: "cron(0 14 ? * MON-FRI *)",
+  job: {
+    handler: "packages/functions/src/cron/prReminders.handler",
+    link: [table],
+  },
+});
+
+export {};
