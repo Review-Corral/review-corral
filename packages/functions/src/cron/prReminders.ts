096b5b0736485ceefb30adefdbde4906252f9391
Alex McLean <alex.mclean25@gmail.com>
2026-01-16 07:13:47 -0800

    Disabling pr reminder cron job

--- packages/functions/src/cron/prReminders.ts
+++ packages/functions/src/cron/prReminders.ts
@@ -11,10 +11,11 @@ import type { ScheduledEvent } from "aws-lambda";
 const LOGGER = new Logger("cron.prReminders");
 
 export async function handler(event: ScheduledEvent): Promise<void> {
-  LOGGER.info("Starting PR reminder job", {
-    time: event.time,
-    resources: event.resources,
-  });
+  // TODO: Re-enable once we add a status field to pull_requests table to track
+  // open/closed/merged state. Currently PRs are never removed from the database
+  // when closed/merged, causing stale PRs to appear in reminders.
+  LOGGER.info("PR reminder job disabled", { time: event.time });
+  return;
 
   const prsResult = await tryCatch(fetchOutstandingPrsForReminders());
 
