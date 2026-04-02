c962fa09e9d6076d2a3083cb5ffacc06abf9d4f8
Alex McLean <alex.mclean25@gmail.com>
2026-04-01 21:01:42 -0700

    Fixing migration

--- packages/domain/postgres/migrations/0012_bitter_killmonger.sql
+++ packages/domain/postgres/migrations/0012_bitter_killmonger.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "pull_requests" ADD COLUMN "reviewer_statuses" jsonb DEFAULT '[]'::jsonb NOT NULL;
+ALTER TABLE "pull_requests" ADD COLUMN IF NOT EXISTS "reviewer_statuses" jsonb DEFAULT '[]'::jsonb NOT NULL;
